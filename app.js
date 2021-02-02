a684b2937c90bb40f3e886757dfd162d8aec7fa2
Eduardo <ing.valen0421@hotmail.com>
2021-02-01 22:03:41 -0400

    Creando action, store y reducer, suscribiendome

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 //// Tienda de video juegos, online
 //// Conceptos basicos
 
+// import { createStore } from "redux"; //react
+const createStore = require('redux').createStore;
+
 ////Actions
 
 // Una accion de comprar, de devolver algo al almacen y asi
@@ -20,7 +23,28 @@
 // De la action pasa al store la solicitud y de ahi pasa al reducer para que el como empleado actue
 
 
+//Reducer
+const default_games_state = {
+    pokemon: 10
+}
 
+const games_reducer = (state = default_games_state, action) => {
+    switch (action.type) {
+        default: return state;
+    }
+};
 
+//Store
+const store = createStore(games_reducer);
+console.log('Estadp inicial', store.getState());
 
+// cuando un cliente compra una unidad se cambia el estado en la app, y el store envia un evento, para eso hay que subscribirse a la store
+//por cada cambio que se realice, cada cambio se refleja aqui
+store.subscribe(() => {
+    console.log('Cambio de estado:', store.getState());
+})
 
+//enviar mensaje por el action
+//con dispatch
+
+store.dispatch();
