32befce92fd776823214afb5c7af225fc283f20d
Eduardo <ing.valen0421@hotmail.com>
2021-02-01 21:50:25 -0400

    Teoria sobre Acions, Reducers and Store

--- app.js
+++ app.js
@@ -3,16 +3,23 @@
 
 ////Actions
 
-
+// Una accion de comprar, de devolver algo al almacen y asi
 
 ///// Reducers
 
-Los trabajadores del almacen
+// Los trabajadores del almacen que ordenan las cajitas en el almacen
+// Cuando llega un cliente eso es una action
 
 
 //// Store
 
-Donde se guarda lo del almacen (de la aplicacion)
+// Donde se guarda lo del almacen (de la aplicacion)
+// Cuaqndo un store este se llenara de muchos reducers
+
+
+// De la action pasa al store la solicitud y de ahi pasa al reducer para que el como empleado actue
+
+
 
 
 
