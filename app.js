74b035868aa950cd1b8fd7f8d9523c5e44f18240
Eduardo <ing.valen0421@hotmail.com>
2021-02-01 20:37:54 -0400

    no message

---
+++ app.js
@@ -0,0 +1,19 @@
+//// Tienda de video juegos, online
+//// Conceptos basicos
+
+////Actions
+
+
+
+///// Reducers
+
+Los trabajadores del almacen
+
+
+//// Store
+
+Donde se guarda lo del almacen (de la aplicacion)
+
+
+
+
