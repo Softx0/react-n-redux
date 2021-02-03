f94c9b028c99046151a370980cad5f2b98556532
Eduardo <ing.valen0421@hotmail.com>
2021-02-03 19:15:34 -0400

    Curso finalizado! excelente!

--- practica-redux-react/src/components/buscador/ResultadoPokemon.js
+++ practica-redux-react/src/components/buscador/ResultadoPokemon.js
@@ -1,16 +1,26 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
 
 const ResultadoPokemon = () => {
+
+    const buscador = useSelector((state) => state.buscador);
+
+    //Ocultar paso a paso cada parte
     return (
+
         <div>
             <h3 className="text-white" >Resultado: </h3>
-            <div className="text-warning" >Buscando...</div>
-            <div className="text-success">
-                <img src="" alt="pokemon" />
-                <span>Pikachu</span>
-            </div>
-            <span className="text-danger" >Error</span>
+            {buscador.loading && <div className="text-warning" >Buscando...</div>}
+
+            {
+                buscador.pokemon.length >= 1 &&
+                <div className="text-success">
+                    <img src={buscador.pokemon[0].sprites.front_default} alt="Pokemon" />
+                    <span>{buscador.pokemon[0].name}</span>
+                </div>
+            }
+
+            {buscador.error !== '' && <span className="text-danger" >{buscador.error}</span>}
         </div>
     );
 }
