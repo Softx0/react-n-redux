c1487e0c8033b6c7e9eaf1a1b46a6593bda85c67
Eduardo <ing.valen0421@hotmail.com>
2021-02-03 08:14:52 -0400

    Maquetacion, imagen, y css

--- practica-redux-react/src/App.js
+++ practica-redux-react/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap/dist/css/bootstrap.css';
+import pokeImage from './img/pokemonImage.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <div className="row" >
+        <div className="col-12" >
+          <div className="card mt-5" style={{ maxWidth: '370px' }} >
+            <div className="row no-gutters" >
+              <div className="col-4" >
+                <img src={pokeImage} alt="pokemon" className="card-img" />
+              </div>
+              <div className="col-8" >
+                <div className="card-body" >
+                  <div className="card-title h3 text-center" >
+                    unidades 30
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
