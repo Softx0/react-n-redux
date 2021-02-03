14f92b876c49b589456fdf7327396cddd450208a
Eduardo <ing.valen0421@hotmail.com>
2021-02-03 10:43:03 -0400

    Conectando redux con react, gracias al Provider, asi tambien, utilizandolo directamente en el maquetado :D
    Ya finalizando, seteando el store en nuestro provider

--- practica-redux-react/src/App.js
+++ practica-redux-react/src/App.js
@@ -5,35 +5,38 @@ import pokeImage from './img/pokemonImage.png';
 import CantidadPokemon from './components/CantidadPokemon';
 import CompraPokemon from './components/CompraPokemon';
 import store from './redux/store/store';
+import { Provider } from 'react-redux';
+
+
 
 function App() {
 
-  console.log(store.getState());
-
   return (
-    <div className="App container">
-      <div className="row" >
-        <div className="col-12" >
-          <div className="card mt-5" style={{ maxWidth: '370px' }} >
-            <div className="row no-gutters" >
-              <div className="col-4" >
-                <img src={pokeImage} alt="pokemon" className="card-img" />
-              </div>
-              <div className="col-8" >
-                <div className="card-body" >
-                  <div className="card-title h3 text-center" >
-                    <CantidadPokemon />
-                    <CompraPokemon />
+    <Provider store={store} >
+      <div className="App container">
+        <div className="row" >
+          <div className="col-12" >
+            <div className="card mt-5" style={{ maxWidth: '370px' }} >
+              <div className="row no-gutters" >
+                <div className="col-4" >
+                  <img src={pokeImage} alt="pokemon" className="card-img" />
+                </div>
+                <div className="col-8" >
+                  <div className="card-body" >
+                    <div className="card-title h3 text-center" >
+                      <CantidadPokemon />
+                      <CompraPokemon />
+                    </div>
                   </div>
                 </div>
               </div>
+
             </div>
 
           </div>
-
         </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
